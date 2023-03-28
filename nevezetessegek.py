905a5690b42db3934c19a9257f8b2418e0e059c7
Nagy Kristóf <76815314+KristOOFF@users.noreply.github.com>
2023-03-28 21:23:40 +0200

    Add files via upload

--- nevezetessegek.py
+++ nevezetessegek.py
@@ -1,3 +1,53 @@
-csv = open("nevezetessegek.csv", "r", encoding="UTF-8")
-print(csv.read())
+class Nevezetesseg:
+    def __init__(self, nev: str, orszag: str, varos: str, epult: str) -> None:
+        self.nev = nev
+        self.orszag = orszag
+        self.varos = varos
+        self.epult = epult
 
+    def __str__(self) -> str:
+        return f"{self.nev} {self.orszag} {self.varos}"
+
+csv = open("./nevezetessegek.csv", encoding="UTF-8")
+
+sorok = csv.readlines()
+
+sorok.pop(0)
+
+nevezetessegek = []
+
+for sor in sorok:
+    tulajdonsagok = sor.split(";")
+    nevezetesseg = Nevezetesseg(tulajdonsagok[0], tulajdonsagok[1], tulajdonsagok[2], tulajdonsagok[3])
+    nevezetessegek.append(nevezetesseg)
+
+valasztas = -1
+
+while valasztas != 4:
+    print("1. Nevezetességek listája")
+    print("2. Ország választás")
+    print("3. Város választás")
+    print("4. Kilépés")
+
+    valasztott_szam = input("Válassz egy számot 1-4: ")
+
+    if not valasztott_szam.isnumeric():
+        continue
+
+    valasztas = int(valasztott_szam)
+
+    if valasztas == 1:
+        for nevezetesseg in nevezetessegek:
+          print(nevezetesseg)
+    
+    elif valasztas == 2:
+        orszagnev = input("Adj meg egy országot: ")
+        for nevezetesseg in nevezetessegek:
+            if orszagnev == nevezetesseg.orszag:
+                print(nevezetesseg.nev)
+
+    elif valasztas == 3:
+        varosnev = input("Adj meg egy várost: ")
+        for nevezetesseg in nevezetessegek:
+            if varosnev == nevezetesseg.varos:
+                print(f"{nevezetesseg.nev} {nevezetesseg.orszag}")
